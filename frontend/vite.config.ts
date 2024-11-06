0f395b8bd03651cff1ebba8b324c07c13454c8df
MajdSoubh <majdsoubh53@gmail.com>
2024-11-06 17:59:09 +0300

    install @iconscout/react-unicons and @types/node and refactore aliases implementation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@images": path.resolve(__dirname, "./src/assets/images"),
       "@styles": path.resolve(__dirname, "./src/assets/styles"),
       "@pages": path.resolve(__dirname, "./src/pages"),
-      "@Components": path.resolve(__dirname, "./src/components"),
+      "@components": path.resolve(__dirname, "./src/components"),
     },
   },
 });
