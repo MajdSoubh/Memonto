a21811c3f45361ace4b2f029cce00cf04fd24c6c
MajdSoubh <majdsoubh53@gmail.com>
2024-11-06 14:57:20 +0300

    Initiate the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./assets/style/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-18%" }}></div>
+    </div>
+  );
+}
+
+export default App;
