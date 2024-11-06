ea65fe78c7c66355177540cb0a1b77852bd08103
MajdSoubh <majdsoubh53@gmail.com>
2024-11-06 15:46:15 +0300

    change the App style file name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import "./assets/style/App.css";
-
+import "@styles/app.css";
+import Home from "@pages/Home.jsx";
 function App() {
   return (
     <div className="app">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-18%" }}></div>
+      <Home />
     </div>
   );
 }
