418793e93af53220f95042f86a6d661ebddf5cbb
MajdSoubh <majdsoubh53@gmail.com>
2024-12-14 17:34:01 +0300

    update routes list

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -3,16 +3,42 @@ import { validator } from "../middleware/validator.js";
 import { checkUserIdRule } from "../rules/user/CheckUserIdRule.js";
 import { auth } from "../middleware/Auth.js";
 import { updateUserRule } from "../rules/user/UpdateUserRule.js";
-import { updateUser } from "../controllers/user/UserController.js";
+import { followUserRule } from "../rules/user/FollowUserRule.js";
+import {
+  deleteUser,
+  followUser,
+  getAllUsers,
+  getFollowingSuggestion,
+  getUser,
+  searchForUser,
+  unfollowUser,
+  updateUser,
+} from "../controllers/user/UserController.js";
 import multer from "multer";
 import storage from "../bootstrap/Storage.js";
+import { querySearchRule } from "../rules/user/QuerySearchRule.js";
 
 const router = express.Router();
 const upload = multer({ storage: storage.profile });
 
 router.get("/", auth("user"), getAllUsers);
+router.get("/following-suggestions", auth("user"), getFollowingSuggestion);
 router.get("/:id", [auth("user"), validator(checkUserIdRule)], getUser);
-
+router.post(
+  "/search",
+  [auth("user"), validator(querySearchRule)],
+  searchForUser
+);
+router.put(
+  "/follow/:id",
+  [auth("user"), validator(followUserRule)],
+  followUser
+);
+router.put(
+  "/unfollow/:id",
+  [auth("user"), validator(followUserRule)],
+  unfollowUser
+);
 router.put(
   "/",
   [
@@ -25,5 +51,6 @@ router.put(
   ],
   updateUser
 );
+router.delete("/", auth("user"), deleteUser);
 
 export default router;
