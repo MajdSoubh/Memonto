600d1b9ae615122f4da7b537f795be41a2f62c81
MajdSoubh <majdsoubh53@gmail.com>
2024-12-14 17:44:51 +0300

    implementation to add new messages

---
+++ backend/controllers/user/MessageController.js
@@ -0,0 +1,18 @@
+import { dispatchEvent } from "../../events/Events.js";
+import { Message } from "../../models/Message.js";
+
+export const addMessage = async (req, res) => {
+  const { conversationId, receiverId, text } = req.body;
+
+  const message = new Message({
+    conversationId,
+    sender: req.user.id,
+    text,
+  });
+
+  const result = await message.save();
+
+  dispatchEvent(receiverId, "receiveMessage", result);
+
+  res.status(200).json(result);
+};
