b6ff1eee9cc3366dac465b50486bc5d05792a3fa
MajdSoubh <majdsoubh53@gmail.com>
2024-12-15 00:50:35 +0300

    add message input component

---
+++ frontend/src/components/sections/messenger/MessageInput.jsx
@@ -0,0 +1,42 @@
+import { useContext, useState } from "react";
+import Card from "../../partials/Card.jsx";
+import InputEmoji from "react-input-emoji";
+import { MessengerContext } from "../../../Contexts/MessengerContext";
+import { PlusIcon } from "../../../assets/icons/icons.jsx";
+const MessageInput = ({ className }) => {
+  const messenger = useContext(MessengerContext);
+  const [message, setMessage] = useState("");
+  const handleChange = (message) => {
+    setMessage(message);
+  };
+  const handleSend = () => {
+    messenger.handleSendMessage(message);
+    setMessage("");
+  };
+  return (
+    <Card
+      className={
+        " !h-[3rem] !gap-3 !rounded-3xl flex !flex-row items-center justify-center " +
+        className
+      }
+    >
+      <PlusIcon />
+      <InputEmoji
+        value={message}
+        placeholder="Type your message"
+        onEnter={() => message.trim().length !== 0 && handleSend()}
+        onChange={handleChange}
+        inputClass="w-full focus:border-orange text-sm rounded-3xl  "
+      />
+      <button
+        onClick={handleSend}
+        disabled={message.trim().length === 0}
+        className="p-2 px-4 text-center text-xs button rounded-[1rem] font-bold"
+      >
+        Send
+      </button>
+    </Card>
+  );
+};
+
+export default MessageInput;
