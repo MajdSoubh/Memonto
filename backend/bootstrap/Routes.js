02524ed28f8b59fbf9a18839d5a6057946c27ded
MajdSoubh <majdsoubh53@gmail.com>
2024-12-14 17:19:38 +0300

    register users routes

--- backend/bootstrap/Routes.js
+++ backend/bootstrap/Routes.js
@@ -1,8 +1,10 @@
 import authRoutes from "../routes/Auth.js";
+import userRoutes from "../routes/User.js";
 
 // Register app routes
 const registerRoutes = (app) => {
   app.use("/", authRoutes);
+  app.use("/users", userRoutes);
 };
 
 export default registerRoutes;
