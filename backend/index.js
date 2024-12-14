8ae0cd3947a002a5cd5d3be7cf21a43bc43e5991
MajdSoubh <majdsoubh53@gmail.com>
2024-12-14 17:14:52 +0300

    set routes registering in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import config from "config";
 import express from "express";
 import registerMiddlewares from "./bootstrap/Middlewares.js";
+import registerRoutes from "./bootstrap/Routes.js";
 import { registerSocket } from "./bootstrap/Socket.js";
 import "./bootstrap/Database.js";
 
 const app = express();
 
 registerMiddlewares(app);
+registerRoutes(app);
 registerSocket(app);
 const appURL = config.get("app.URL");
 const appPort = config.get("app.port");
