22029672a03f9059d406a2eb84ace5622a2d4640
MajdSoubh <majdsoubh53@gmail.com>
2024-12-14 17:43:04 +0300

    register post routes

--- backend/bootstrap/Routes.js
+++ backend/bootstrap/Routes.js
@@ -1,10 +1,16 @@
 import authRoutes from "../routes/Auth.js";
 import userRoutes from "../routes/User.js";
+import postRoutes from "../routes/Post.js";
+import express from "express";
+import path from "path";
 
 // Register app routes
 const registerRoutes = (app) => {
   app.use("/", authRoutes);
   app.use("/users", userRoutes);
+  app.use("/posts", postRoutes);
+  // Return static files from 'public' directory.
+  app.use("/public", express.static(path.join(path.resolve(), "public")));
 };
 
 export default registerRoutes;
---
+++ backend/routes/Post.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { validator } from "../middleware/validator.js";
+import {
+  createPost,
+  deletePost,
+  fetchUserPosts,
+  getPost,
+  getTimelinePosts,
+  reactToPost,
+  updatePost,
+} from "../controllers/user/PostController.js";
+import { auth } from "../middleware/Auth.js";
+import { updatePostRule } from "../rules/post/UpdatePostRule.js";
+import { checkPostIdRule } from "../rules/post/CheckPostIdRule.js";
+import multer from "multer";
+import storage from "../bootstrap/Storage.js";
+import { createPostRule } from "../rules/post/CreatePostRule.js";
+import { checkUserIdRule } from "../rules/user/CheckUserIdRule.js";
+
+const router = express.Router();
+
+const upload = multer({ storage: storage.post });
+
+router.get("/timeline", auth("user"), getTimelinePosts);
+router.get(
+  "/users/:id",
+  [auth("user"), validator(checkUserIdRule)],
+  fetchUserPosts
+);
+router.get("/:id", [auth("user"), validator(checkPostIdRule)], getPost);
+router.put(
+  "/react/:id",
+  [auth("user"), validator(checkPostIdRule)],
+  reactToPost
+);
+router.post(
+  "/",
+  [auth("user"), upload.array("images[]"), validator(createPostRule)],
+  createPost
+);
+router.put("/:id", [auth("user"), validator(updatePostRule)], updatePost);
+router.delete("/:id", [auth("user"), validator(checkPostIdRule)], deletePost);
+
+export default router;
