13401a5a51eb32fd7433f101e09aa3fb7f7dbf1c
MajdSoubh <majdsoubh53@gmail.com>
2024-12-15 14:06:42 +0300

    change the position of the blur circle

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 function AuthLayout() {
   return (
     <div className="p-4 text-black overflow-hidden bg-[#f3f3f3] h-dvh ">
-      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
-      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <div className="circle -top-[30%] right-[0%]"></div>
+      <div className="circle bottom-[10%] -left-[30%]"></div>
       <Outlet />
     </div>
   );
