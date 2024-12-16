8135f77e1605a0066aff8686e08cd8cd04c5ebef
MajdSoubh <majdsoubh53@gmail.com>
2024-12-16 13:44:51 +0300

    enhance search input in mobile UI

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -4,7 +4,7 @@ import FollowersCard from "../components/cards/FollowersCard";
 const Search = () => {
   return (
     <div className="flex flex-col gap-3">
-      <div className="relative flex justify-center items-center bg-white p-3 rounded-xl ">
+      <div className="relative flex justify-center items-center border hover:shadow-inputShadow hover:border-transparent border-slate-400 bg-white p-3 rounded-xl ">
         <UsersSearchInput />
       </div>
       <FollowersCard />
